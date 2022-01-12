9b28aa66aa0548ea63465b879934213090e0bdc2
Kevin Chen <kc3585@nyu.edu>
2022-01-12 08:27:17 -0500

    modify for entrypoit

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,5 @@
 
 profiles="";
 
-if [ ! -z "$ENABLE_KAFKA_MODE" ] && [ "$ENABLE_KAFKA_MODE" = true ];
-then
-  profiles="kafka";
-fi
-
-if [ ! -z "$ENABLE_RABBITMQ_MODE" ] && [ "$ENABLE_RABBITMQ_MODE" = true ];
-then
-  if [ $profiles = "" ];
-  then
-    profiles="rabbitmq";
-  else
-    profiles=$profiles",rabbitmq";
-  fi
-fi
-
 java -jar -Dspring.profiles.active=$profiles compiler.jar
 

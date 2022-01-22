899069975fd3e12fbaf6b9d4054982f8dc5c1320
Kevin Chen <kc3585@nyu.edu>
2022-01-22 15:16:59 -0500

    added root health check fix

--- src/main/java/com/cp/compiler/controllers/Root.java
+++ src/main/java/com/cp/compiler/controllers/Root.java
@@ -1,22 +1,19 @@
 package com.cp.compiler.controllers;
 
-import com.cp.compiler.exceptions.CompilerServerException;
-import com.cp.compiler.models.Language;
-import com.cp.compiler.models.PostBody;
 import com.cp.compiler.models.Response;
-import com.cp.compiler.services.CompilerService;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import org.springframework.http.MediaType;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/")
 public class Root {
-    @GetMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/")
     @ApiOperation(value = "Target Group Health Check", notes = "Health Check", response = Response.class)
     public ResponseEntity<String> compilePython() {
+        log.info("Root health check passed");
         return ResponseEntity.ok().body("Health check passed");
     }
 }
